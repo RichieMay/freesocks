4e8bda160c943c366f2b4c64e2cfcdfda86edb45
RichieMay <myanbu@163.com>
2018-09-04 23:35:30 +0800

    update protocol

--- source/xxtea_repeater.h
+++ source/xxtea_repeater.h
@@ -8,18 +8,19 @@
 #include <boost/date_time.hpp>
 #include <boost/thread/mutex.hpp>
 
+/*
+*	+-------------------------------------------------------------------------+
+*	|  4 bytes  |  4 bytes  |  2 bytes  |  (dataLen+3)/4*4 bytes  |  4 bytes  |
+*	+-----------+-----------+-----------+-------------------------------------+
+*	|   total   | timestamp | checksum  |     data + fill data    |  dataLen  |
+*	+-------------------------------------------------------------------------+
+*/
+
 class xxtea_repeater : public repeater
 {
 	#define MX (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)))
 
-
-	/*
-	* ┌────────────┐────────────┐────────────────────────┐─────────────────┐─────────────────┐
-	* │   4 bytes  │   2 bytes  │ (dataLen+3)/4*4 bytes  │       4 bytes   │     4 bytes     │
-	* │────────────│────────────│────────────────────────│─────────────────│─────────────────│
-	* │    total   │  checksum  │    data + fill data    │      dataLen    │   timestamp     │
-	* └────────────┘────────────┘────────────────────────┘─────────────────┘─────────────────┘
-	*/
+	typedef boost::uint32_t key_t[4];
 public:
 	xxtea_repeater(const std::string& proxy_host, boost::uint16_t proxy_port, const std::string& secret)
 		: proxy_host_(proxy_host), proxy_port_(proxy_port), secret_(secret)
@@ -44,33 +45,36 @@ public:
 		*dst = new boost::uint8_t[dstLen];
 		boost::uint8_t* p = *dst;
 
-		memcpy(p, &dstLen, sizeof(boost::uint32_t));// total length
+		memcpy(p, &dstLen, sizeof(boost::uint32_t));// total
 		p += sizeof(boost::uint32_t);
 
-		boost::uint16_t checksum = crc16_check(dstLen);
-		memcpy(p, &checksum, sizeof(boost::uint16_t));// total length checksum
+		boost::uint32_t timestamp = boost::posix_time::microsec_clock::universal_time().time_of_day().total_microseconds() & 0xffffffff;
+		memcpy(p, &timestamp, sizeof(boost::uint32_t));// timestamp
+		p += sizeof(boost::uint32_t);
+
+		key_t key;
+		obfuscation_key(timestamp, key);
+
+		boost::uint16_t checksum = crc16_check(key, dstLen);
+		memcpy(p, &checksum, sizeof(boost::uint16_t));// checksum
 		p += sizeof(boost::uint16_t);
 
 		memcpy(p, src, srcLen); // copy source
 
-		boost::uint32_t cipherLen = dstLen - sizeof(boost::uint32_t) - sizeof(boost::uint16_t);
-		memcpy(p + cipherLen - sizeof(boost::uint32_t) * 2, &srcLen, sizeof(boost::uint32_t)); //source length 
+		boost::uint32_t cipherLen = dstLen - sizeof(boost::uint32_t) * 2 - sizeof(boost::uint16_t);
+		memcpy(p + cipherLen - sizeof(boost::uint32_t), &srcLen, sizeof(boost::uint32_t)); //source length 
 
-		boost::uint32_t timestamp = boost::posix_time::microsec_clock::universal_time().time_of_day().total_microseconds() & 0xffffffff;
-		memcpy(p + cipherLen - sizeof(boost::uint32_t), &timestamp, sizeof(boost::uint32_t)); //source length
-
-		blur((boost::uint32_t*)p, (cipherLen - sizeof(boost::uint32_t) * 2)/ sizeof(boost::uint32_t), timestamp, true);
-
-		boost::int32_t n = cipherLen / sizeof(boost::uint32_t);//exclude srcLen、checksum
-		btea((boost::uint32_t*)p, n, key_);
+		boost::int32_t n = cipherLen / sizeof(boost::uint32_t);
+		btea((boost::uint32_t*)p, n, key);
 
 		return srcLen;
 	}
 
 	int decrypt(boost::uint8_t* src, boost::uint32_t srcLen, boost::uint8_t** dst, boost::uint32_t& dstLen)
 	{
+		key_t key;
 		boost::uint32_t totalLen = 0;
-		int ret = get_decrypt_length(src, srcLen, totalLen);
+		int ret = get_decrypt_length(src, srcLen, totalLen, key);
 		if (ret != err_success)
 		{
 			return ret;
@@ -81,20 +85,17 @@ public:
 			return err_no_more;
 		}
 
-		boost::uint32_t cipherLen = totalLen - sizeof(boost::uint32_t) - sizeof(boost::uint16_t);
+		boost::uint32_t cipherLen = totalLen - sizeof(boost::uint32_t) * 2 - sizeof(boost::uint16_t);
 		*dst = new boost::uint8_t[cipherLen]; //exclude srcLen、checksum
 		boost::uint8_t* p = *dst;
 
-		src += ((sizeof(boost::uint32_t) + sizeof(boost::uint16_t)));
+		src += ((sizeof(boost::uint32_t) * 2 + sizeof(boost::uint16_t)));
 		memcpy(p, src, cipherLen); //copy cipher
 
 		boost::int32_t n = cipherLen / sizeof(boost::uint32_t);//exclude srcLen、checksum
-		btea((boost::uint32_t*)p, -n, key_);
+		btea((boost::uint32_t*)p, -n, key);
 		
-		boost::uint32_t timestamp = *((boost::uint32_t*)(p + cipherLen - sizeof(boost::uint32_t)));
-		blur((boost::uint32_t*)p, (cipherLen - sizeof(boost::uint32_t) * 2) / sizeof(boost::uint32_t), timestamp, false);
-
-		p += (cipherLen - sizeof(boost::uint32_t) * 2);
+		p += (cipherLen - sizeof(boost::uint32_t));
 		dstLen = *((boost::uint32_t*)p);
 		return (totalLen == get_encrypt_length(dstLen) ? totalLen : err_unknown);
 	}
@@ -109,7 +110,7 @@ private:
 	void string2key(const std::string& key)
 	{
 		MD5 md5(key);
-		memcpy(key_, md5.byte_digest(), sizeof(key_));
+		memcpy(key_, md5.byte_digest(), sizeof(key_t));
 	}
 
 	void btea(boost::uint32_t *v, boost::int32_t n, boost::uint32_t* key)
@@ -155,30 +156,35 @@ private:
 		}
 	}
 
-	boost::uint16_t crc16_check(boost::uint32_t totalLen)
+	boost::uint16_t crc16_check(const key_t &key, boost::uint32_t totalLen)
 	{
 		boost::crc_16_type crc16;
-		crc16.process_bytes((boost::uint8_t*)key_, sizeof(key_));
+		crc16.process_bytes((boost::uint8_t*)key, sizeof(key_t));
 		crc16.process_bytes((boost::uint8_t*)(&totalLen), sizeof(boost::uint32_t));
 		return crc16.checksum();
 	}
 
 	boost::uint32_t get_encrypt_length(boost::uint32_t srcLen)
 	{
-		return	10 + (srcLen + 7) / 4 * 4;
+		return 14 + (srcLen + 3) / 4 * 4;
 	}
 
-	int get_decrypt_length(boost::uint8_t* data, boost::uint32_t dataLen, boost::uint32_t& totalLen)
+	int get_decrypt_length(boost::uint8_t* data, boost::uint32_t dataLen, boost::uint32_t& totalLen, key_t& key)
 	{
-		if (dataLen < (sizeof(boost::uint32_t) + sizeof(boost::uint16_t)))
+		if (dataLen < (sizeof(boost::uint32_t) * 2 + sizeof(boost::uint16_t)))
 		{
 			return err_no_more;
 		}
 		
-		totalLen = *((boost::uint32_t*)data); //total length
+		totalLen = *((boost::uint32_t*)data); //total
 		data += sizeof(boost::uint32_t);
-		boost::uint16_t checksum = *((boost::uint16_t*)data); //total length checksum
-		if (checksum != crc16_check(totalLen))
+
+		boost::uint32_t timestamp = *((boost::uint32_t*)data); //timestamp
+		data += sizeof(boost::uint32_t);
+
+		obfuscation_key(timestamp, key);
+		boost::uint16_t checksum = *((boost::uint16_t*)data); //checksum
+		if (checksum != crc16_check(key, totalLen))
 		{
 			return err_unknown;
 		}
@@ -188,30 +194,19 @@ private:
 		}
 	}
 
-	void blur(boost::uint32_t *data, boost::uint32_t dataLen, boost::uint32_t timestamp, bool is_add)
+	void obfuscation_key(const boost::uint32_t& timestamp, key_t& key)
 	{
-		if (is_add)
+		boost::uint8_t* p = (boost::uint8_t*)(&timestamp);
+		for (boost::uint8_t i = 0; i < 4; i++)
 		{
-			while (dataLen--)
-			{
-				*data = (*data) + timestamp;
-				data++;
-			}
-		}
-		else
-		{
-			while (dataLen--)
-			{
-				*data = (*data) - timestamp;
-				data++;
-			}
+			key[i] = key_[i] + p[i];
 		}
 	}
 
 private:
+	key_t key_;
 	boost::mutex lock_;
 	std::string secret_;
-	boost::uint32_t key_[4];
 	std::string proxy_host_;
 	boost::uint16_t proxy_port_;
 };
